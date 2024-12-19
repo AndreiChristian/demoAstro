628530821db1e40927d10649bcb227d764cdf7ca
Andrei <andreichristiannetoiu@gmail.com>
2024-12-19 17:15:30 +0200

    x

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/infopages/brand.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Finfopages%2Fbrand.mdx&astroContentModuleFlag=true")]]);
+		
